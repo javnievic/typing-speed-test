0a57a326bb75de168cf09cdca1029887fe7cd14a
javnievic <javiernieto03.jnv@gmail.com>
2026-03-02 17:52:33 +0100

    feat: update button classes for improved styling and functionality in typing test

--- script.js
+++ script.js
@@ -8,17 +8,35 @@ fetch('./data.json')
     .then(data => {
         textsData = data;
         console.log(textsData);
-        initDifficultyButtons()
+        initDifficultyButtons();
+        initModeButtons(); 
     })
 
-function initDifficultyButtons() {
-    const buttons = document.querySelectorAll('.selection-btn')
-    console.log(buttons)
+function initSelectionButton(classSelector, onSelect) {
+    const buttons = document.querySelector(classSelector).querySelectorAll('.selection-btn'); 
+
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
             buttons.forEach(btn => btn.classList.remove('active'));
             btn.classList.add('active'); 
+            onSelect(btn);
         })
     })
+}
 
+function initDifficultyButtons() {
+    initSelectionButton('.difficulty', (btn) => {
+        currentDifficulty = btn.dataset.difficulty; 
+    })
+}
+
+function initModeButtons() {
+    initSelectionButton('.mode', (btn) => {
+        currentMode = btn.dataset.mode; 
+    })
+}
+
+
+function startTest () {
+    console.log(textsData[currentDifficulty]) 
 }
